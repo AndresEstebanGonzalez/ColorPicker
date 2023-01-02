3052e7a5bfdab062ecdd55bf4d4b40cdbdbeb929
AndresEstebanGonzalez <74146741+AndresEstebanGonzalez@users.noreply.github.com>
2023-01-02 16:52:27 -0300

    Comments fixed.

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
-let genBtn = document.getElementById("colorBtn");
-let colorCircle = document.getElementById("colorCircle");
+const genBtn = document.getElementById("colorBtn");
+const colorCircle = document.getElementById("colorCircle");
 
-let minNumb = 1;
-let maxNumb = 255;
-let randomNumber = Math.random();
+const minNumb = 1;
+const maxNumb = 255;
+const randomNumber = Math.random();
 
 // Random number 1-255
-function randNumb() {
-  let randomR = Math.floor(
+function generateNumber() {
+  return Math.floor(
     Math.random() * (randomNumber * (maxNumb - minNumb + 1) + minNumb)
   );
-  let randomG = Math.floor(
-    Math.random() * (randomNumber * (maxNumb - minNumb + 1) + minNumb)
-  );
-  let randomB = Math.floor(
-    Math.random() * (randomNumber * (maxNumb - minNumb + 1) + minNumb)
-  );
+}
 
+function randNumb() {
   //RGB Variable for background
-  let rgbValue = randomR + "," + randomG + "," + randomB;
+  let rgbValue =
+    generateNumber() + "," + generateNumber() + "," + generateNumber();
   let rgbVariable = "rgb" + "(" + rgbValue + ")";
   colorCircle.style.backgroundColor = rgbVariable;
 }
 
 //Button clicked
-function btnClicked() {
-  genBtn.addEventListener("click", (e) => {
-    randNumb();
-  });
-}
-
-btnClicked();
+genBtn.addEventListener("click", (e) => {
+  randNumb();
+});
